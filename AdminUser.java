6abbeb10d556d5ab5c579a517a0441be465d0a66
Matthew Bolan <Bolan.Matthew.MB@Gmail.com>
2021-09-30 18:37:10 -0500

    first commit

---
+++ AdminUser.java
@@ -0,0 +1,159 @@
+package com.caresoft.clinicapp;
+
+import java.util.ArrayList;
+
+import java.util.Date;
+
+
+public class AdminUser extends User implements PHICompliantUser,PHIAdminCompliant {
+	
+    private Integer employeeID;
+    
+    private String role;
+    
+    private ArrayList<String> securityIncidents;
+    
+    public AdminUser() {}
+    
+    public AdminUser (Integer id, int pin, String role, Integer employeeID) {
+    	
+    	super (id, pin);
+    	
+    	this.setRole(role);
+    	
+    	this.employeeID = employeeID;
+    	
+    }
+    
+    public void newIncident(String notes) {
+    	
+        String report = String.format(
+        		
+            "Datetime Submitted: %s \n,  Reported By ID: %s\n Notes: %s \n", 
+            
+            new Date(), this.id, notes
+            
+        );
+        
+        securityIncidents.add(report);
+        
+    }
+    
+    public void authIncident() {
+    	
+        String report = String.format(
+        		
+            "Datetime Submitted: %s \n,  ID: %s\n Notes: %s \n", 
+            
+            new Date(), this.id, "AUTHORIZATION ATTEMPT FAILED FOR THIS USER"
+            
+        );
+        
+        securityIncidents.add(report);
+        
+    }
+    
+
+
+
+	@Override
+	
+	public boolean assignPin(int pin) {
+		
+		if (pin > 999999 || pin < 100000) {
+			
+			return false;
+		}
+		
+		return true;
+		
+		}
+	
+
+	
+	@Override
+	
+	public boolean isAuthorized (Integer confirmedAuthID) {
+		
+		authIncident ();
+		
+		if ( confirmedAuthID == this.getId()) {
+			
+			return true;
+			
+		}
+			
+		else {
+			
+			return false;
+		
+		}
+		
+		}
+	
+	
+	
+	@Override
+	
+	public ArrayList<String> reportSecurityIncidents() {
+		
+		return securityIncidents;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+
+	public Integer getEmployeeID() {
+		
+		return employeeID;
+		
+	}
+
+	public void setEmployeeID(Integer employeeID) {
+		
+		this.employeeID = employeeID;
+		
+	}
+
+	public String getRole() {
+		
+		return role;
+		
+	}
+
+	public void setRole(String role) {
+		
+		this.role = role;
+		
+	}
+
+	public ArrayList<String> getSecurityIncidents() {
+		
+		return securityIncidents;
+		
+	}
+
+	public void setSecurityIncidents(ArrayList<String> securityIncidents) {
+		
+		this.securityIncidents = securityIncidents;
+		
+	}
+	
+	
+	
+	
+	
+	
+}
+	
+		
+		
+	
+	
+	
+	
---
+++ Patient.java
@@ -0,0 +1,196 @@
+package com.caresoft.clinicapp;
+
+import java.util.ArrayList;
+
+import java.util.Date;
+
+public class Patient extends User implements PHICompliantUser {
+	
+	private String role;
+    
+    private String firstName;
+    
+    private String lastName;
+    
+    // .. other personal identifying information .. //
+    
+    Physician primaryCarePhysician;
+    
+    ArrayList<Integer> currentPrescriptionsByRX;
+    
+    int providerCode;
+    
+    int memberNumber;
+    
+    PatientRecord medicalHistory;
+    
+    // TO DO: Constructor
+    
+    public Patient() {}
+    
+    public Patient (Integer id, int pin, String role, String firstName, String lastName) {
+    	
+	    	super (id, pin);
+	    	
+	    	this.setRole(role);
+	    	
+	    	this.firstName = firstName;
+	    	
+	    	this.lastName = lastName;
+    	
+    }
+
+	@Override
+
+	public boolean assignPin(int pin) {
+	
+		if (pin > 999999 || pin < 100000) {
+		
+			return false;
+		}
+	
+		return true;
+	
+		}
+
+
+
+	@Override
+	
+	public boolean isAuthorized (Integer confirmedAuthID) {
+		
+		if ( confirmedAuthID == this.getId()) {
+			
+			return true;
+			
+		}
+			
+		else {
+			
+			return false;
+		
+		}
+		
+		}
+	
+	
+    
+    public boolean requestAppointment(Date date, Physician doctor) {
+    	
+        boolean successfullyAdded = false;
+        
+        // you see existing code to find and schedule an appointment
+        
+    	// (Leave as is for the assignment, no need to implement anything here.)
+        
+        return successfullyAdded;
+        
+    }
+    
+    void addChartNotes(String notes) {
+    	
+        this.medicalHistory.getCharts().add(notes);
+        
+    }
+
+	public String getRole() {
+		
+		return role;
+		
+	}
+
+	public void setRole(String role) {
+		
+		this.role = role;
+		
+	}
+
+	public String getFirstName() {
+		
+		return firstName;
+		
+	}
+
+	public void setFirstName(String firstName) {
+		
+		this.firstName = firstName;
+		
+	}
+
+	public String getLastName() {
+		
+		return lastName;
+		
+	}
+
+	public void setLastName(String lastName) {
+		
+		this.lastName = lastName;
+		
+	}
+
+	public Physician getPrimaryCarePhysician() {
+		
+		return primaryCarePhysician;
+		
+	}
+
+	public void setPrimaryCarePhysician(Physician primaryCarePhysician) {
+		
+		this.primaryCarePhysician = primaryCarePhysician;
+		
+	}
+
+	public ArrayList<Integer> getCurrentPrescriptionsByRX() {
+		
+		return currentPrescriptionsByRX;
+		
+	}
+
+	public void setCurrentPrescriptionsByRX(ArrayList<Integer> currentPrescriptionsByRX) {
+		
+		this.currentPrescriptionsByRX = currentPrescriptionsByRX;
+		
+	}
+
+	public int getProviderCode() {
+		
+		return providerCode;
+		
+	}
+
+	public void setProviderCode(int providerCode) {
+		
+		this.providerCode = providerCode;
+		
+	}
+
+	public int getMemberNumber() {
+		
+		return memberNumber;
+		
+	}
+
+	public void setMemberNumber(int memberNumber) {
+		
+		this.memberNumber = memberNumber;
+		
+	}
+
+	public PatientRecord getMedicalHistory() {
+		
+		return medicalHistory;
+		
+	}
+
+	public void setMedicalHistory(PatientRecord medicalHistory) {
+		
+		this.medicalHistory = medicalHistory;
+		
+	}
+    
+    
+    
+    
+    
+}
